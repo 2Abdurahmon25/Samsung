b2b9179b2ab5f27328d89a838f2109c8d27ffddb
abdurahmon <a23bdurahm0n118@gmail.com>
2026-07-18 10:43:15 +0500

    NIGGER

--- app/page.jsx
+++ app/page.jsx
@@ -1113,7 +1113,7 @@ export default function page() {
         style={{
           padding: "96px 24px",
           background: "#050505",
-          height: "130dvh",
+          height: "120dvh",
         }}
       >
         <div style={{ maxWidth: "1280px", margin: "0 auto", width: "100%" }}>
